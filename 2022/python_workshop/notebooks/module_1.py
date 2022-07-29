22f8e7830471ade5004111a7d3593d1450a1311b
edward <volgyesi.edward@gmail.com>
2022-07-29 12:50:42 +0300

    tuple

--- 2022/python_workshop/notebooks/module_1.py
+++ 2022/python_workshop/notebooks/module_1.py
@@ -1,11 +1,14 @@
 a = 1
 
-print(f"NAME from module_1.py: {__name__}")
-print(f"FILE from module_1.py: {__file__}")
-print(f"module_1 -- a = {a}")
+# print(f"NAME from module_1.py: {__name__}")
+# print(f"FILE from module_1.py: {__file__}")
+# print(f"module_1 -- a = {a}")
 
 b = 2
 
 c = 3
 
-
+if __name__ == '__main__':
+    print("Don't do that!")
+else:
+    print('All good')
